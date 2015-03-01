677f5e13209d019c1fba4a5392d13eef66de70a5
James Timms PC <James-Timms@hotmail.com>
2015-03-01 18:51:26 +0000

    Got the shaders to work finally1

--- Demos/Personal/Demo2.java
+++ Demos/Personal/Demo2.java
@@ -17,5 +17,4 @@ public class Demo2 {
         SimpleRenderer.init();
         SimpleRenderer.loop();
     }
-
 }
--- src/org/FRPengine/rendering/SimpleRenderer.java
+++ src/org/FRPengine/rendering/SimpleRenderer.java
@@ -2,6 +2,7 @@ package org.FRPengine.rendering;
 
 import org.FRPengine.core.FRPDisplay;
 import org.FRPengine.core.Time;
+import org.FRPengine.maths.Vector3f;
 import org.FRPengine.rendering.shaders.BasicShader;
 import org.FRPengine.rendering.shaders.Material;
 import sodium.Listener;
@@ -10,6 +11,8 @@ import sodium.StreamSink;
 import static org.lwjgl.glfw.GLFW.glfwPollEvents;
 import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
 import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL20.*;
 
 /**
  * Created by TekMaTek on 26/02/2015.
@@ -20,6 +23,9 @@ public class SimpleRenderer {
     static BasicShader shader = new BasicShader();
     private static Listener renderLoopListener;
 
+    private static int vbo;
+    private static int size;
+    
     public static void loop() {
 
         StreamSink<Long> renderLoop = new StreamSink<>();
@@ -37,7 +43,8 @@ public class SimpleRenderer {
         glfwPollEvents();
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        RenderSimpleSquare();
+//        RenderSimpleSquare();
+        draw();
 
         glfwSwapBuffers(FRPDisplay.GetWindow());
         glfwPollEvents();
@@ -46,11 +53,32 @@ public class SimpleRenderer {
     public static void init() {
         System.out.println(RenderingUtil.GetOpenGLVersion());
         glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
+        vbo = glGenBuffers();
+        size = 0;
+
+        Vertex[] data = new Vertex[] {
+                new Vertex(new Vector3f(-1.0f, -1.0f, 0.0f)),
+                new Vertex(new Vector3f(1.0f, -1.0f, 0.0f)),
+                new Vertex(new Vector3f(0.0f, 1.0f, 0.0f))
+        };
+        addVertices(data);
     }
 
-    public static void RenderSimpleSquare() {
+    public static void draw() {
         shader.Bind();
         shader.updateUniforms(Material.WhiteNoTexture());
-        cube.draw();
+        glEnableVertexAttribArray(0);
+
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glVertexAttribPointer(0, 3, GL_FLOAT, false, Vertex.SIZE * 4, 0);
+        glDrawArrays(GL_TRIANGLES, 0, size);
+
+        glDisableVertexAttribArray(0);
+    }
+
+    public static void addVertices(Vertex[] vertices) {
+        size = vertices.length * Vertex.SIZE;
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, RenderingUtil.createFlippedBuffer(vertices), GL_STATIC_DRAW);
     }
 }
