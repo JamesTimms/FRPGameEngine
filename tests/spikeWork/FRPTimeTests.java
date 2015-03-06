c6566e94752be504d106ed43551925240abf2da9
JamesLaptop <james-timms@hotmail.com>
2015-03-06 14:57:54 +0000

    Made the time stream test more accuate.

--- tests/spikeWork/FRPTimeTests.java
+++ tests/spikeWork/FRPTimeTests.java
@@ -25,6 +25,16 @@ public class FRPTimeTests {
         return (a + range) > b && (a - range) < b;
     }
 
+    public static boolean ApproaxEquals(float a, float b, int significantFigures) {
+        float difference = Math.abs(a - b);
+        float max = Math.max(Math.abs(a), Math.abs(b));
+        double sigfigs = Math.pow(0.1d, significantFigures);
+        double margin = sigfigs
+                * max;
+        boolean result = difference < margin;
+        return result;
+    }
+
     @Test
     public void testTimeDelay() {
         StreamSink<Integer> timeStream = new StreamSink<>();
@@ -40,14 +50,13 @@ public class FRPTimeTests {
                     timeNT[0] = System.nanoTime();
                     delta[0] = timeNT[0] - lastTime[0];
                     lastTime[0] = timeNT[0];
-                    System.out.println("test: " + (float) delta[0] / Time.SECOND + "   real: " + time);
-                    assertTrue(ApproxEqualsRange((float) delta[0] / Time.SECOND, time, (float) 1000000 / Time.SECOND));//3 DP to the right must match
+//                    System.out.println("test: " + (float) delta[0] / Time.SECOND + "   real: " + time);
+                    assertTrue(ApproaxEquals((float) delta[0] / Time.SECOND, time, 2));
                 });
         for(int i = 0; i < 1000000; i++) {
             timeStream.send(Time.THIRTY_PER_SECOND);
         }
         timeListener.unlisten();
     }
-
     //TODO: Test how long it takes to execute the code over imperative code.
 }
