6c6456319c6fcbd221ecc7448b41dfbe459de83f
James Timms PC <James-Timms@hotmail.com>
2015-03-18 02:33:18 +0000

    Working on trying to get more functional like code in Demo4

--- Demos/Personal/Demo4.java
+++ Demos/Personal/Demo4.java
@@ -5,7 +5,6 @@ import org.engineFRP.FRP.Time;
 import org.engineFRP.Physics.collision.Click;
 import org.engineFRP.core.FRPDisplay;
 import org.engineFRP.core.FRPKeyboard;
-import org.engineFRP.core.FRPMouse;
 import org.engineFRP.core.Transform;
 import org.engineFRP.maths.Vector3f;
 import org.engineFRP.rendering.MeshUtil;
@@ -17,8 +16,7 @@ import sodium.Listener;
 import sodium.Stream;
 import sodium.Tuple2;
 
-import static org.engineFRP.core.FRPMouse.cursorPosStream;
-import static org.engineFRP.core.FRPMouse.screenToWorldSpace;
+import static org.engineFRP.core.FRPMouse.*;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.glClear;
@@ -36,18 +34,18 @@ public class Demo4 {
     private static Time pollTimer = new Time(120);
 
     private static Shader shader2;
-    private static Transform[] sceneMeshes;
+    private static Transform[] sceneTransforms;
 
     public Demo4() {
         FRPDisplay.create();
         FRPKeyboard.create();
-        FRPMouse.create();
+        create();
         SimpleRenderer.init();
         loop();
     }
 
     public void setupScene() {
-        for(Transform transform : sceneMeshes) {
+        for(Transform transform : sceneTransforms) {
             transform.mergeIntoCellAndAccum(movements());
         }
     }
@@ -63,25 +61,33 @@ public class Demo4 {
 
     Listener scoreListener;
 
+    public static boolean anyShapesClicked(Tuple2<Mouse, Cursor> mouse) {
+        boolean hasClicked = false;
+        for(Transform transform : sceneTransforms) {
+            hasClicked = hasClicked
+                    || Click.isInPolygon(transform.addTransformPosAndFlipY(), screenToWorldSpace(mouse.b.position));
+        }
+        return hasClicked;
+    }
+
     public void loop() {
         shader2 = new BasicShader();
-        sceneMeshes = new Transform[] {
+        sceneTransforms = new Transform[] {
                 new Transform(new Vector3f(0.0f, 0.0f, -1.0f), MeshUtil.BuildSquare())
         };
         setupScene();
 
-        Cell<Integer> score = FRPMouse.clickStream
+        Cell<Integer> score = clickStream
                 .filter(mouse -> mouse.button == GLFW_MOUSE_BUTTON_LEFT &&
                         mouse.action == GLFW_PRESS)
                 .snapshot(cursorPosStream.hold(null), (click, cursor) -> new Tuple2<>(click, cursor))
-                .map(mouse -> Click
-                        .isInPolygon(sceneMeshes[0].addTransformPosAndFlipY(), screenToWorldSpace(mouse.b.position)))
+                .map(Demo4::anyShapesClicked)
                 .map(hitShape -> {
                     if(hitShape) {
-                        sceneMeshes[0].mesh.resize(0.80f);
+                        sceneTransforms[0].mesh.resize(0.80f);
                         return 1;
                     } else {
-                        sceneMeshes[0].mesh.resize(1.25f);
+                        sceneTransforms[0].mesh.resize(1.25f);
                         return -1;
                     }
                 })
@@ -110,7 +116,7 @@ public class Demo4 {
     }
 
     public static void drawDemo3() {
-        for(Transform transform : sceneMeshes) {
+        for(Transform transform : sceneTransforms) {
             shader2.draw(transform);
         }
     }
